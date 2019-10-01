9ca46441f510f0b7e19675062b0b34f86db656f3
Brian Simonton <bsimonton78@gmail.com>
2019-10-01 19:40:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ main.remove();
 
 const newHeader = document.createElement('h1');
 newHeader.id = "victory";
-newHeader.innerText = "Brian is the champion!";
+newHeader.innerText = "Brian is the champion";
 document.body.appendChild(newHeader);
